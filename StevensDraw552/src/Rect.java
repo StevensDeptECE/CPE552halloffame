9237f86571832e2d9634195f5d4b73168507b738
abhisangani <46908951+abhisangani@users.noreply.github.com>
2019-04-02 17:14:08 -0400

    Add files via upload

--- StevensDraw552/src/Rect.java
+++ StevensDraw552/src/Rect.java
@@ -1,16 +1,15 @@
-/**
- * @author dkruger
- */
+
+
 
 import java.awt.*;
 public class Rect extends Shape {
-    private int w, h;
+    private int w1, h1;
     public Rect(int x, int y, int w, int h) {
         super(x,y);
-        this.w = w; this.h = h;
+        this.w1 = w; this.h1 = h;
     }
     @Override
     public void draw(Graphics g) {
-        g.drawRect(x, y, w,h);
+        g.drawRect(x, y, w1-x, h1-y);
     }
 }
