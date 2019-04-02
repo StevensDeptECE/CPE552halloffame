05693346aba66c44641d31e6bf13109528dccccb
Tyler Bryk <22185354+tylerbryk@users.noreply.github.com>
2019-04-02 17:22:59 -0400

    Add files via upload

--- StevensDraw552/src/Star.java
+++ StevensDraw552/src/Star.java
@@ -1,17 +1,27 @@
-
-
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Star extends Shape {
-    private int x2, y2;
+    private int x2, y2, level = 5;
+    private ArrayList<Integer> points;
     public Star(int x, int y, int x2, int y2) {
         super(x,y);
         this.x2 = x2; this.y2 = y2;
+        
+        points.add(x + distance(x,y,x2,y2)); // Top Vertex
+        points.add(y + distance(x,y,x2,y2)); // Top Vertex
+        
+        
     }
+    
+    private int distance(int x, int y, int x2, int y2) {
+        return (int)Math.sqrt(((x2-x)*(x2-x))+((y2-y)*(y2-y)));
+    }
+    
     @Override
     public void draw(Graphics g) {
-        // Code here...
-        
-        g.drawLine(x, y, x2, y2);
+        for (int i = 1; i <= level; i++) {
+            g.drawLine(x, y, x2, y2);
+        }
     }   
 }
