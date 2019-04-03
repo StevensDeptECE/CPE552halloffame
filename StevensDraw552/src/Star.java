3dd9d24529922c10bb104beb7c6674da26e83a07
Tyler Bryk <22185354+tylerbryk@users.noreply.github.com>
2019-04-02 20:42:06 -0400

    Add files via upload

--- StevensDraw552/src/Star.java
+++ StevensDraw552/src/Star.java
@@ -1,31 +1,37 @@
 import java.awt.*;
+import static java.lang.Math.*;
 import java.util.ArrayList;
 
 public class Star extends Shape {
     private int x2, y2, numPoints = 5;
     private int[] coords = new int[numPoints*2];
+    private int dx = x2-x, dy = y2-y;
     public Star(int x, int y, int x2, int y2) {
         super(x,y);
         this.x2 = x2; this.y2 = y2;
+        if (dx == 0) dx = 1;
+        if (dy == 0) dy = 1;
         int r = distance(x,y,x2,y2);
-        double degree = Math.atan((y2 - y) / (x2 - x));
+        double degree = atan(dy / dx);
         
         coords[0] = (x2); // Top Vertex
         coords[1] = (y2); // Top Vertex
-        degree += 72;
-        coords[2] = (int)(x + Math.cos(degree) * r);
-        coords[3] = (int)(y + Math.sin(degree) * r);
-        degree += 72;
-        coords[4] = (int)(x + Math.cos(degree) * r);
-        coords[5] = (int)(y + Math.sin(degree) * r);
-        degree += 72;
-        coords[6] = (int)(x + Math.cos(degree) * r);
-        coords[7] = (int)(y + Math.sin(degree) * r);
-        degree += 72;
-        coords[8] = (int)(x + Math.cos(degree) * r);
-        coords[9] = (int)(y + Math.sin(degree) * r);
+        degree += 72*PI/180;
+        coords[2] = (int)(x + cos(degree) * r);
+        coords[3] = (int)(y + sin(degree) * r);
+        
+        degree += 72*PI/180;
+        coords[4] = (int)(x + cos(degree) * r);
+        coords[5] = (int)(y + sin(degree) * r);
+        degree += 72*PI/180;
+        coords[6] = (int)(x + cos(degree) * r);
+        coords[7] = (int)(y + sin(degree) * r);
+        degree += 72*PI/180;
+        coords[8] = (int)(x + cos(degree) * r);
+        coords[9] = (int)(y + sin(degree) * r);
         
         System.out.println("   ");
+        System.out.println(degree);
         System.out.println("X: " + x + " Y: " +y);
         System.out.println("x2: " + x2+ " Y2: "+y2);
         for (int i = 0; i < coords.length; i++) {
@@ -35,15 +41,15 @@ public class Star extends Shape {
     }
     
     private int distance(int x, int y, int x2, int y2) {
-        return (int)Math.sqrt(((x2-x)*(x2-x))+((y2-y)*(y2-y)));
+        return (int)sqrt(((x2-x)*(x2-x))+((y2-y)*(y2-y)));
     }
     
     @Override
     public void draw(Graphics g) {
-        g.drawLine(coords[0], coords[1], coords[2], coords[3]);
-        g.drawLine(coords[2], coords[3], coords[4], coords[5]);
-        g.drawLine(coords[4], coords[5], coords[6], coords[7]);
-        g.drawLine(coords[6], coords[7], coords[8], coords[9]);
-        g.drawLine(coords[8], coords[9], coords[0], coords[1]);
+        g.drawLine(coords[0], coords[1], coords[4], coords[5]);
+        g.drawLine(coords[4], coords[5], coords[8], coords[9]);
+        g.drawLine(coords[8], coords[9], coords[2], coords[3]);
+        g.drawLine(coords[2], coords[3], coords[5], coords[7]);
+        g.drawLine(coords[5], coords[7], coords[0], coords[1]);
     }   
 }
