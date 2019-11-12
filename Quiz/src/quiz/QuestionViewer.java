8cf9298a82bb4fe1093d88ee6e73380a94178550
Peihong Yu <peihongyu1996@gmail.com>
2019-11-12 17:43:16 -0500

    update QuestionViewer.java

--- Quiz/src/quiz/QuestionViewer.java
+++ Quiz/src/quiz/QuestionViewer.java
@@ -1,26 +1,49 @@
 package quiz;
+
 import java.awt.*;
 import javax.swing.*;
+
 /**
  *
  * @author dkruger
  */
 public class QuestionViewer extends JPanel {
-    private Quiz quiz;        // current quiz, null if not taking one
-    private Student student;  // student logged in
-    private double score;     // score of the current quiz
-    private int questionNum;
-    public QuestionViewer( ) {
-    }
-    public void setQuiz(Quiz q) {
-        quiz = q;
-        score = 0;
-        questionNum = 0;
-    }
-    public void paint(Graphics g) {
-        var q = quiz.getQuestions().get(questionNum);
-        String question = q.getQuestion();
-        g.drawString(question, 15, 100);
-    }
-    
+	private Quiz quiz; // current quiz, null if not taking one
+	private Student student; // student logged in
+	private double score; // score of the current quiz
+	private int questionNum;
+
+	public QuestionViewer() {
+	}
+
+	public void setQuiz(Quiz q) {
+		quiz = q;
+		score = 0;
+		questionNum = 0;
+	}
+
+	public Quiz getQuiz() {
+		return this.quiz;
+	}
+	
+	public int getQuestionNum() {
+		return questionNum;
+	}
+	public void QuestionNumplusone() {
+		questionNum++;
+	}
+	public void QuestionNumminusone() {
+		questionNum--;
+	}
+	
+	public void paint(Graphics g) {
+		var q = quiz.getQuestions().get(questionNum);
+		String question = q.getQuestion();
+		g.drawString(question, 15, 100);
+
+//		q = quiz.getQuestions().get(questionNum + 1);
+//		question = q.getQuestion();
+//		g.drawString(question, 15, 200);
+	}
+
 }
