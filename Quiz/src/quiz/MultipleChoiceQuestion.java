620a0617ece1b5f422588d0422d86eed095be3a0
edytioco <edytioco@stevens.edu>
2019-11-15 16:48:34 -0500

    added array list of answers

--- Quiz/src/quiz/MultipleChoiceQuestion.java
+++ Quiz/src/quiz/MultipleChoiceQuestion.java
@@ -1,19 +1,40 @@
 package quiz;
-
+import java.util.*;
 /**
  * @author edytioco
  */
 public class MultipleChoiceQuestion extends Question {
-    private Answer answer;
-
+    ArrayList<String> answerList = new ArrayList<>();
+    Answer correct;
+    int correctIndex;
+    // print out question
+    // print out potential answers
+    
+    
+    
+    // Answer: make an array of choices, with one identified as true
+    
+    // user has to enter his choice
+        // make this a string input
+    
+    // ID what answer is correct is correct
+    
+    
+    
+    
     @Override
     public Answer getAnswer() {
-        return answer;
+        correct.setText(answerList.get(correctIndex)); // TODO: figure out how to set correct.
+        return correct;
     }
 
     @Override
     public void setAnswer(Answer a) {
-        answer = a;
+        answerList.add(correctIndex, a.getText());
     }
     
+    // populate the 
+    public void setWrongAnswers(Answer a){
+        
+    }
 }
